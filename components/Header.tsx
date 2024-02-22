cf554ff19613dc9c217bf85155569f595cf6f81a
Hanner Moraes <101107412+hannermoraes@users.noreply.github.com>
2024-02-21 22:22:48 -0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
     <div className="container w-full flex justify-center mt-10 mb-10">
-      <h1 className="from-neutral-700 font-bold text text-4xl">Trade Control</h1>
+      <h1 className="from-neutral-700 font-bold text text-4xl">Gerenciado de Trades</h1>
     </div>
   )
 }
